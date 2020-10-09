a641b611a3326a592e185c086d98dd4ebf170e7b
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-09 20:03:37 +0200

    Clarify block not implemented error

--- components/Switch.js
+++ components/Switch.js
@@ -4,5 +4,5 @@ import Tag from "./Tag";
 export default function Switch({ children, test }) {
     const correctChild = children.find((child) => child.props.test === test);
   
-    return correctChild ?? <Tag>Block type implemented</Tag>;
+    return correctChild ?? <Tag>Block type "{test}" not implemented</Tag>;
 }

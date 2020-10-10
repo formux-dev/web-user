010ddc263ec6ead9cdeefbb1bc239a2a0db2adf7
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-10 14:04:55 +0200

    Extracted Block component and cleaned up Form

---
+++ components/Block.js
@@ -0,0 +1,82 @@
+import styled, { css } from "styled-components";
+
+import Switch from "./Switch";
+
+import CheckboxInput from "./blocks/CheckboxInput";
+import Description from "./blocks/Description";
+import MultipleChoiceInput from "./blocks/MultipleChoiceInput";
+import ParagraphInput from "./blocks/ParagraphInput";
+import ShortInput from "./blocks/ShortInput";
+
+export default function Block({ block, index, globalDebug, value, onChange }) {
+    const [debug, setDebug] = useState(false);
+
+    return (
+        <BlockContainer isSeparated={globalDebug}>
+        <Switch test={block.type}>
+            <Description test="description" block={block} />
+            <ShortInput
+            test="shortinput"
+            block={block}
+            value={value}
+            onChange={(value) => onChange({ key: block.key, value })}
+            />
+            <ParagraphInput
+            test="paragraphinput"
+            block={block}
+            value={value}
+            onChange={(value) => onChange({ key: block.key, value })}
+            />
+            <MultipleChoiceInput
+            test="multiplechoiceinput"
+            block={block}
+            value={value}
+            onChange={(value) => onChange({ key: block.key, value })}
+            />
+            <CheckboxInput
+            test="checkboxinput"
+            block={block}
+            value={value}
+            onChange={(value) => onChange({ key: block.key, value })}
+            />
+        </Switch>
+
+        {globalDebug && (
+            <RawData>
+            <div>
+                <input
+                type="checkbox"
+                id={"debug" + index}
+                value={debug}
+                onChange={(e) => setDebug(e.target.checked)}
+                />
+                <label htmlFor={"debug" + index}> Show raw data</label>
+            </div>
+
+            {debug && <code>Block: {beautify(block, null, 2, 80)}</code>}
+            </RawData>
+        )}
+        </BlockContainer>
+    );
+}
+
+const BlockContainer = styled.div`
+  margin-bottom: 24px;
+
+  ${(props) =>
+    props.isSeparated &&
+    css`
+      border: 2px solid rgba(0, 0, 0, 0.2);
+      border-radius: 8px;
+      padding: 16px 12px;
+      margin-bottom: 8px;
+    `};
+`;
+
+const RawData = styled.div`
+  padding-top: 8px;
+  border-top: 2px solid rgba(0, 0, 0, 0.2);
+  margin-top: 8px;
+  width: 100%;
+  white-space: pre-wrap;
+`
--- components/Form.js
+++ components/Form.js
@@ -1,15 +1,8 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { useEffect, useState } from "react";
 import beautify from "json-beautify";
 
-import CheckboxInput from "./blocks/CheckboxInput";
-import Description from "./blocks/Description";
-import MultipleChoiceInput from "./blocks/MultipleChoiceInput";
-import ParagraphInput from "./blocks/ParagraphInput";
-import ShortInput from "./blocks/ShortInput";
-
 import Navbar from "./Navbar";
-import Switch from "./Switch";
 import Tag from "./Tag";
 
 export default function Form({ formID }) {
@@ -30,6 +23,26 @@ function FormFields({ formID }) {
   const [userData, setUserData] = useState({});
   const [globalDebug, setGlobalDebug] = useState(false);
 
+  const generateInitialUserData = (data) => {
+    return Object.assign(
+      {},
+      ...data.blocks.map((block) =>
+        block.key
+          ? {
+              [block.key]: block.data.responseIsArray ? [] : "",
+            }
+          : null
+      )
+    )
+  }
+
+  const handleBlockChange = ({ key, value }) => {
+    setUserData((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  }
+
   useEffect(() => {
     fetch(
       `https://us-central1-formux-8d67b.cloudfunctions.net/form?formID=${formID}`
@@ -38,22 +51,8 @@ function FormFields({ formID }) {
       .then((data) => {
         console.log("Got data from function", data);
 
-        setTimeout(() => {
-          // Inital values
-          setUserData(
-            Object.assign(
-              {},
-              ...data.blocks.map((block) =>
-                block.key
-                  ? {
-                      [block.key]: block.data.responseIsArray ? [] : "",
-                    }
-                  : null
-              )
-            )
-          );
-          setFormData(data);
-        }, 500);
+        setUserData(generateInitialUserData(data))
+        setFormData(data);
       });
   }, []);
 
@@ -79,12 +78,7 @@ function FormFields({ formID }) {
             key={index}
             index={index}
             globalDebug={globalDebug}
-            onChange={({ key, value }) => {
-              setUserData((prevState) => ({
-                ...prevState,
-                [key]: value,
-              }));
-            }}
+            onChange={handleBlockChange}
             value={userData[block.key]}
           />
         ))
@@ -102,58 +96,6 @@ function FormFields({ formID }) {
 }
 
 
-
-function Block({ block, index, globalDebug, value, onChange }) {
-  const [debug, setDebug] = useState(false);
-
-  return (
-    <FormField isSeparated={globalDebug}>
-      <Switch test={block.type}>
-        <Description test="description" block={block} />
-        <ShortInput
-          test="shortinput"
-          block={block}
-          value={value}
-          onChange={(value) => onChange({ key: block.key, value })}
-        />
-        <ParagraphInput
-          test="paragraphinput"
-          block={block}
-          value={value}
-          onChange={(value) => onChange({ key: block.key, value })}
-        />
-        <MultipleChoiceInput
-          test="multiplechoiceinput"
-          block={block}
-          value={value}
-          onChange={(value) => onChange({ key: block.key, value })}
-        />
-        <CheckboxInput
-          test="checkboxinput"
-          block={block}
-          value={value}
-          onChange={(value) => onChange({ key: block.key, value })}
-        />
-      </Switch>
-
-      {globalDebug && (
-        <RawData>
-          <div>
-            <input
-              type="checkbox"
-              id={"debug" + index}
-              value={debug}
-              onChange={(e) => setDebug(e.target.checked)}
-            />
-            <label htmlFor={"debug" + index}> Show raw data</label>
-          </div>
-
-          {debug && <code>Block: {beautify(block, null, 2, 80)}</code>}
-        </RawData>
-      )}
-    </FormField>
-  );
-}
 
 
 const Wrapper = styled.div`
@@ -175,7 +117,6 @@ const Content = styled.div`
 `;
 
 
-
 const Main = styled.main`
   display: flex;
   flex-direction: column;
@@ -191,27 +132,3 @@ const Main = styled.main`
   }
 `;
 
-const FormField = styled.div`
-  margin-bottom: 24px;
-
-  ${(props) =>
-    props.isSeparated &&
-    css`
-      border: 2px solid rgba(0, 0, 0, 0.2);
-      border-radius: 8px;
-      padding: 16px 12px;
-      margin-bottom: 8px;
-
-      &:nth-last-child() {
-        border: none;
-      }
-    `};
-`;
-
-const RawData = styled.div`
-  padding-top: 8px;
-  border-top: 2px solid rgba(0, 0, 0, 0.2);
-  margin-top: 8px;
-  width: 100%;
-  white-space: pre-wrap;
-`

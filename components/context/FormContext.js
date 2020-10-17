dd4ad46b813bd3cc843d3772400871b6b6ae3286
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-17 15:42:09 +0200

    Remove formData from context, and useMemo wasn't needed

--- components/context/FormContext.js
+++ components/context/FormContext.js
@@ -3,34 +3,18 @@ import { createContext, useState, useMemo } from "react";
 const FormContext = createContext({});
 
 function FormProvider({ children }) {
-  const [formData, setFormData] = useState({});
   const [userData, setUserData] = useState({});
   const [rating, setRating] = useState(null);
   const [isDebug, setIsDebug] = useState(false);
 
-  // const value = {
-  //   formData,
-  //   setFormData,
-  //   userData,
-  //   setUserData,
-  //   isDebug,
-  //   setIsDebug,
-  //   rating,
-  //   setRating,
-  // };
-
-  const value = useMemo(() => {
-    return {
-      formData,
-      setFormData,
-      userData,
-      setUserData,
-      isDebug,
-      setIsDebug,
-      rating,
-      setRating,
-    };
-  }, [formData, userData, isDebug, rating]);
+  const value = {
+    userData,
+    setUserData,
+    isDebug,
+    setIsDebug,
+    rating,
+    setRating,
+  };
 
   return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
 }

029503a420201ec7b6a3bd247369b097d30b54c2
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-18 17:20:36 +0200

    Add rating to debug

--- components/Debug.js
+++ components/Debug.js
@@ -6,11 +6,16 @@ import beautify from "json-beautify";
 import { FormContext } from "./context/FormContext";
 
 export default function Debug() {
-  const { isDebug, setIsDebug, userData } = useContext(FormContext);
+  const { isDebug, setIsDebug, userData, rating } = useContext(FormContext);
   useHotkeys("ctrl+q", () => setIsDebug(prev => !prev));
 
   if (isDebug) {
-    return <WhiteSpace>Userdata: {beautify(userData, null, 2, 80)}</WhiteSpace>;
+    return (
+      <React.Fragment>
+        <WhiteSpace>Userdata: {beautify(userData, null, 2, 80)}</WhiteSpace>
+        <WhiteSpace>Rating: {String(rating)}</WhiteSpace>
+      </React.Fragment>
+    );
   } else {
     return false;
   }
